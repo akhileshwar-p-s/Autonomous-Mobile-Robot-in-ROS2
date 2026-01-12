dd755bfc274e887fe143b364b03b7bc851e678b6
Akhileshwar Pratap Singh <145456372+akhileshwar-p-s@users.noreply.github.com>
2026-01-12 10:46:59 +0530

    Create launch file for robot simulation setup

---
+++ displau.launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    
+    # Get URDF via xacro
+    robot_description_content = Command([
+        'xacro ', 
+        PathJoinSubstitution([
+            FindPackageShare('robot_pkg'),
+            'urdf',
+            'my_diffbot.urdf.xacro'
+        ])
+    ])
+    
+    robot_description = {'robot_description': robot_description_content}
+    
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[robot_description]
+    )
+    
+    # Joint State Publisher GUI
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui'
+    )
+    
+    # RViz
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', PathJoinSubstitution([
+            FindPackageShare('robot_pkg'),
+            'rviz',
+            'display.rviz'
+        ])]
+    )
+    
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz_node
+    ])
